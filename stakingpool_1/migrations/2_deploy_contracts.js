68a7ecf0fbadfbc9fcf7b3b90251d8541d864361
Prajwal M G <34264682+prajwalmg7@users.noreply.github.com>
2021-04-17 13:33:16 +0530

    Add files via upload

---
+++ stakingpool_1/migrations/2_deploy_contracts.js
@@ -0,0 +1,15 @@
+const Stakingpool = artifacts.require("Stakingpool");
+const MCHToken =artifacts.require("MCHToken");
+const MCFToken = artifacts.require("MCFToken");
+
+module.exports = async function (deployer) {
+  
+  await deployer.deploy(MCHToken);
+  const mch = await MCHToken.deployed();
+  
+  await deployer.deploy(MCFToken);
+  const mcf= await MCFToken.deployed();
+  
+  await deployer.deploy(Stakingpool,mch.address,mcf.address);
+  const pool = await Stakingpool.deployed();
+}
